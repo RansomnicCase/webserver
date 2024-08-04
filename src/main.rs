94b043bcf350cf68068ea92fddd6c92f4044ab16
RansomnicCase <ilovemyth9@gmail.com>
2024-08-04 12:50:58 +0530

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs 	new file:   static/index.html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use actix_files as fs;
 use actix_web::{web, App, HttpServer, Responder};
+use log::info;
 
 async fn hello() -> impl Responder {
     "simple web server in Rust"
@@ -8,12 +10,18 @@ async fn about() -> impl Responder {
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("server is running on port 8080");
+    std::env::set_var("Rust_log", "info");
+    env_logger::init();
     let port = 8080;
+    info!("server is running on port {}", port);
+
+    //println!("server is running on port 8080"); replaced this by using env logger
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(hello))
             .route("/about", web::get().to(about))
+            .service(fs::Files::new("/static", "./static").show_files_listing())
     })
     .bind(format!("127.0.0.1:{}", port))?
     .run()
